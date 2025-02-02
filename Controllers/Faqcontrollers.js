b512e557d0c2eddbeb7601e4bedd35769f7c5be4
sanskar sawant <sanskarsawant23@gmail.com>
2025-02-02 18:12:35 +0530

    Feat:added redis

--- Controllers/Faqcontrollers.js
+++ Controllers/Faqcontrollers.js
@@ -1,6 +1,7 @@
-import { Prisma } from "@prisma/client";
+
 import { PrismaClient } from "@prisma/client";
 import {translate} from '@vitalets/google-translate-api';
+import redis from "../redisClient.js"
 
 
 
@@ -11,12 +12,19 @@ export const getFAQs = async (req, res) =>{
         const prisma = new PrismaClient();
         let faqs = await prisma.fAQ.findMany();
         
-        if(lang && lang !=="en"){
-            faqs = faqs.map((faq)=>({
-                id:faq.id,
-                question:faq[`question_${lang}`] || faq.question,
-                answer:faq[`answer_${lang}`] || faq.answer
-            }))
+        if(lang && lang!=="en"){
+            faqs = await Promise.all(
+                faqs.map(async (faq)=> {
+                    const translatedQuestion = await getTranslation(faq.question, lang);
+                    const translatedAnswerr = await getTranslation(faq.answer, lang);
+
+                    return{
+                        id:faq.id,
+                        question:translatedQuestion,
+                        answer:translatedAnswerr
+                    }
+                })
+            )
         }
 
         res.status(200).json(faqs)
@@ -24,6 +32,19 @@ export const getFAQs = async (req, res) =>{
     }catch(error){
         res.status(500).json({error:"Error Fetching FAQ's"})
     }
+}
+
+const getTranslation =async (text, lang) =>{
+    const cachedTranslation = await redis.get(`${text}_${lang}`);
+    if(cachedTranslation){
+        console.log("Cache hit");
+        return cachedTranslation;
+    }
+
+    console.log("cache miss, translation");
+    const translatedText = await translate(text, {to:lang});
+    await redis.setex(`${text}_${lang}`, 3600, translatedText.text)
+    return translatedText.text
 }
 
 export const createFAQ = async (req, res) =>{
